9d580ba1595edf7b2152755c8b69612b560efc0a
ozatalcs <ozatalcs@rose-hulman.edu>
2018-12-05 13:28:07 -0500

    Conner Ozatalar 5b

--- src/problem0.py
+++ src/problem0.py
@@ -338,7 +338,7 @@ def problem0c(circle, n, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
